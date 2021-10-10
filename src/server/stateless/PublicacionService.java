34f68280d1c11d6b214b73fa66fa618c6fce42b1
Fabricio <fepafumi97@gmail.com>
2021-10-10 02:11:31 -0300

    feat: :sparkles: Crear publicacion de mascota

--- src/server/stateless/PublicacionService.java
+++ src/server/stateless/PublicacionService.java
@@ -4,9 +4,10 @@ import java.util.Collection;
 import java.util.List;
 
 import model.Publicacion;
+import model.Ubicacion;
 
 public interface PublicacionService{
-    public Publicacion create(Publicacion publicacion);
+    public Publicacion create(Publicacion publicacion, Ubicacion ubicacion);
     public List<Publicacion> findAll();
     public Collection <Publicacion> findAllPublicacionUsuario(String id);
     public long getMaxId();
