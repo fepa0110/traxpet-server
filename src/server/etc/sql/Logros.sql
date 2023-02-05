92165412e4fa12aca2684fccc6da601c03de5433
Federico cotrena <nicolascotren@gmail.com>
2023-02-05 20:58:19 -0300

    fix sql logros v1

--- src/server/etc/sql/Logros.sql
+++ src/server/etc/sql/Logros.sql
@@ -1,5 +1,5 @@
-INSERT INTO LOGRO(ID,NIVEL,PREMIO,MAX)VALUES
-(0,1,'10% de descuento Tienda de Mascotas',100),
-(1,2,'20% de descuento Tienda de Mascotas',200),
-(2,3,'30% de descuento Tienda de Mascotas',300),
-(3,4,'40% de descuento Tienda de Mascotas',400);
+INSERT INTO LOGRO(ID, MAX, NIVEL, PREMIO)VALUES
+(0,100,1,'10% de descuento Tienda de Mascotas'),
+(1,200,2,'20% de descuento Tienda de Mascotas'),
+(2,300,3,'30% de descuento Tienda de Mascotas'),
+(3,400,4,'40% de descuento Tienda de Mascotas');
